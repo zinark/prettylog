6cac6ec8a2ee8680647db144931a9a1eb99c619c
beratcarsi <beratcarsi@gmail.com>
2013-11-04 17:15:36 +0200

    refactorings project

---
+++ Web/Core/Interfaces/ILogDao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Web.Core.Dtos;
+
+namespace Web.Core.Interfaces
+{
+    public interface ILogDao
+    {
+        IEnumerable<LogItemDto> Logs(string query, TimeSpan dateRange);
+        void GenerateData();
+    }
+}
